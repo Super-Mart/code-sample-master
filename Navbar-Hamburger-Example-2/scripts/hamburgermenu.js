45ca1f843828e2b6a22091e3ec16dd114b3b13e3
Erik Martinez <ejm20658@gmail.com>
2019-11-21 15:00:58 -0700

    done

--- Navbar-Hamburger-Example-2/scripts/hamburgermenu.js
+++ Navbar-Hamburger-Example-2/scripts/hamburgermenu.js
@@ -5,8 +5,15 @@ const navLinks = document.querySelectorAll(".nav-links li");
 burger.addEventListener("click", () => {
   nav.classList.toggle("nav-inactive");
   nav.classList.toggle("nav-active");
-});
-
-navLinks.forEach((link, index) => {
-  // console.log(index);
+  navLinks.forEach((link, index) => {
+    // console.log(index);
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 +
+        0.5}s`;
+    }
+  });
+  // Burger Animation
+  burger.classList.toggle("toggle");
 });
