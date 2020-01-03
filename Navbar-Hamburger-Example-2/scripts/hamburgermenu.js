8b29f11992705f22cf3eda19b6430a9023808c02
Erik Martinez <ejm20658@gmail.com>
2020-01-03 01:41:12 -0500

    Awesome navbar tutorial

--- Navbar-Hamburger-Example-2/scripts/hamburgermenu.js
+++ Navbar-Hamburger-Example-2/scripts/hamburgermenu.js
@@ -3,7 +3,6 @@ const nav = document.querySelector(".nav-links");
 const navLinks = document.querySelectorAll(".nav-links li");
 
 burger.addEventListener("click", () => {
-  nav.classList.toggle("nav-inactive");
   nav.classList.toggle("nav-active");
   navLinks.forEach((link, index) => {
     // console.log(index);
